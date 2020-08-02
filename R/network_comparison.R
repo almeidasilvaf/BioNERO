e08a2579f9fc27c685f2cdb9516972e0c2fd12b1
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2020-08-02 18:34:07 -0300

    Replaced base R plots and for loops in module_preservation() by lapply and ggplot

--- R/network_comparison.R
+++ R/network_comparison.R
@@ -5,25 +5,33 @@
 #' @param moduleColors Vector of module assignment returned by \code{exp2net}.
 #' @param savePreservation Logical indicating whether to save module preservation into an R object or not. As the calculation of module preservation can take a long time, it is useful to save time in future analyses. Default is TRUE.
 #' @param plot_all_stats Logical indicating whether to save all density and connectivity statistics in a PDF file or not. Default is FALSE.
+#' @param calculateClusterCoeff Logical indicating if clustering coefficient statistics should be calculated. Only valid if \code{plot_all_stats} is TRUE. As it may take a long time, default is FALSE.
 #'
-#' @return Module preservation statistics.
-#' @rdname module_preservation
+#' @return A list object resulting from \code{WGCNA::modulePreservation} and .pdf files with plot statistics.
 #' @seealso
-#'  \code{\link[WGCNA]{modulePreservation}}
+#'  \code{\link[WGCNA]{modulePreservation}},\code{\link[WGCNA]{standardColors}}
+#'  \code{\link[ggpubr]{ggscatter}},\code{\link[ggpubr]{ggarrange}},\code{\link[ggpubr]{ggexport}}
+#'  \code{\link[ggplot2]{theme}},\code{\link[ggplot2]{margin}},\code{\link[ggplot2]{geom_abline}}
+#' @rdname module_preservation
 #' @export
-#' @importFrom WGCNA modulePreservation
+#' @importFrom WGCNA modulePreservation standardColors
+#' @importFrom ggpubr ggscatter ggarrange ggexport
+#' @importFrom ggplot2 theme element_text geom_hline
 module_preservation <- function(exp_ref, exp_test, moduleColors,
-                                savePreservation = TRUE, plot_all_stats = FALSE) {
+                                 savePreservation = TRUE, plot_all_stats = FALSE,
+                                 calculateClusterCoeff = FALSE) {
+
     multiExpr <- list(ref = list(data = t(exp_ref)), test = list(data = t(exp_test)))
     multiColor <- list(ref = moduleColors)
 
     # Calculate module preservation
     pres <- WGCNA::modulePreservation(multiExpr, multiColor,
-                               referenceNetworks = 1,
-                               nPermutations = 200,
-                               randomSeed = 1,
-                               quickCor = 0,
-                               verbose = 3)
+                                      referenceNetworks = 1,
+                                      nPermutations = 200,
+                                      randomSeed = 1,
+                                      quickCor = 0,
+                                      verbose = 3,
+                                      calculateClusterCoeff = calculateClusterCoeff)
 
     if(savePreservation == TRUE) {
         save(pres, file = "modulePreservation.RData");
@@ -52,73 +60,92 @@ module_preservation <- function(exp_ref, exp_test, moduleColors,
     # Main titles for the plot
     mains <- c("Preservation Median rank", "Preservation Zsummary");
 
-    # Start the plot
-    pdf(file="ModulePreservation-Zsummary-medianRank.pdf", wi=10, h=5)
-    par(mfrow = c(1,2))
-    par(mar = c(4.5,4.5,2.5,1))
-    for (p in 1:2)
-    {
-        min <- min(plotData[, p], na.rm = TRUE)
-        max <- max(plotData[, p], na.rm = TRUE)
+    # Plot preservation median rank
+    dplot1 <- data.frame(x=moduleSizes[plotMods], y=plotData[plotMods, 1], label=text, stringsAsFactors=FALSE)
+    p1 <- ggpubr::ggscatter(dplot1, x = "x", y = "y", size=4,
+                            color="black", fill=text, shape=21,
+                            label = "label", repel = TRUE,
+                            xlab = "Module size", ylab = mains[1],
+                            title = mains[1], font.title = c(17, "bold")) +
+        ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
 
-        # Adjust plotting ranges appropriately
-        if (p==2)
-        {
-            if (min > -max/10) min = -max/10
-            ylim <- c(min - 0.1 * (max-min), max + 0.1 * (max-min))
-        } else
-            ylim <- c(max + 0.1 * (max-min), min - 0.1 * (max-min))
-        plot(moduleSizes[plotMods], plotData[plotMods, p], col = 1, bg = modColors[plotMods], pch = 21,
-             main = mains[p],
-             cex = 2.4,
-             ylab = mains[p], xlab = "Module size", log = "x",
-             ylim = ylim,
-             xlim = c(10, 2000), cex.lab = 1.2, cex.axis = 1.2, cex.main =1.4)
-        labelPoints(moduleSizes[plotMods], plotData[plotMods, p], text, cex = 1, offs = 0.08);
+    # Plot preservation Z-summary
+    dplot2 <- data.frame(x=moduleSizes[plotMods], y=plotData[plotMods, 2], label=text, stringsAsFactors=FALSE)
+    p2 <- ggscatter(dplot2, x = "x", y = "y", size=4,
+                    color="black", fill=text, shape=21,
+                    label = "label", repel = TRUE,
+                    xlab = "Module size", ylab = mains[2],
+                    title = mains[2], font.title = c(17, "bold")) +
+        ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+        ggplot2::geom_hline(yintercept = 0) +
+        ggplot2::geom_hline(yintercept = 2, colour = "blue", linetype = 2) +
+        ggplot2::geom_hline(yintercept = 10, colour = "forestgreen", linetype = 2)
 
-        # For Zsummary, add threshold lines
-        if (p==2) {
-            abline(h=0)
-            abline(h=2, col = "blue", lty = 2)
-            abline(h=10, col = "darkgreen", lty = 2)
-        } }
+    fig1 <- ggpubr::ggarrange(p1, p2, ncol=2, nrow=1)
+    ggpubr::ggexport(fig1, filename = "Module_preservation.pdf", height=6, width=12)
 
-    dev.off()
 
     if(plot_all_stats == TRUE) {
+
         # Re-initialize module color labels and sizes
         modColors <- rownames(statsZ)
-        moduleSizes <- mp$quality$Z[[ref]][[test]][, 1];
+        moduleSizes <- pres$quality$Z[[ref]][[test]][, 1];
 
         # Exclude improper modules
         plotMods <- !(modColors %in% c("grey", "gold"));
 
         # Create numeric labels for each module
-        labs <- match(modColors[plotMods], standardColors(50));
+        labs <- match(modColors[plotMods], WGCNA::standardColors(50));
 
-        # Start the plot: open a suitably sized graphical window and set sectioning and margins.
-        pdf(file = "all_Zstats_combined.pdf", onefile = TRUE)
-        par(mfrow = c(3,5))
-        par(mar = c(3,3,2,1))
-        par(mgp = c(1.6, 0.4, 0));
-        # Plot each Z statistic in a separate plot.
-        for (s in 1:ncol(statsZ))
-        {
-            min = min(statsZ[plotMods, s], na.rm = TRUE);
-            max = max(statsZ[plotMods, s], na.rm = TRUE);
-            if (min > -max/5) min = -max/5
-            plot(moduleSizes[plotMods], statsZ[plotMods, s], col = 1, bg = modColors[plotMods], pch = 21,
-                 main = colnames(statsZ)[s],
-                 cex = 1.7,
-                 ylab = colnames(statsZ)[s], xlab = "Module size", log = "x",
-                 ylim = c(min - 0.1 * (max-min), max + 0.1 * (max-min)),
-                 xlim = c(20, 1000))
-            labelPoints(moduleSizes[plotMods], statsZ[plotMods, s], labs, cex = 0.7, offs = 0.04);
-            abline(h=0)
-            abline(h=2, col = "blue", lty = 2)
-            abline(h=10, col = "darkgreen", lty = 2)
+        # Create a list of plots per column of statsZ
+        df <- cbind(moduleSizes[plotMods], statsZ[plotMods, ])
+        colnames(df)[1] <- "module_size"
+        ylabs <- colnames(df)[2:ncol(df)]
+        df$labels <- rownames(df)
+
+        plots <- lapply(ylabs, function(x) ggpubr::ggscatter(df, x = "module_size", y = x, size=4,
+                                                             color="black", fill=labels, shape=21,
+                                                             label = "labels", repel = TRUE,
+                                                             xlab = "Module size", ylab = x,
+                                                             title = x, font.title = c(17, "bold")) +
+                            ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+                            ggplot2::geom_hline(yintercept = 0) +
+                            ggplot2::geom_hline(yintercept = 2, colour = "blue", linetype = 2) +
+                            ggplot2::geom_hline(yintercept = 10, colour = "forestgreen", linetype = 2))
+
+        if(calculateClusterCoeff == FALSE) {
+            pl1 <- plots[1:4]
+            pl2 <- plots[5:8]
+            pl3 <- plots[9:12]
+            pl4 <- plots[13:16]
+            pl5 <- plots[17:19]
+            mpage1 <- ggpubr::ggarrange(plotlist = pl1)
+            mpage2 <- ggpubr::ggarrange(plotlist = pl2)
+            mpage3 <- ggpubr::ggarrange(plotlist = pl3)
+            mpage4 <- ggpubr::ggarrange(plotlist = pl4)
+            mpage5 <- ggpubr::ggarrange(plotlist = pl5)
+            final_list <- list(mpage1, mpage2, mpage3, mpage4, mpage5)
+
+            multipage <- ggpubr::ggarrange(plotlist = final_list, ncol=1, nrow=1)
+            ggpubr::ggexport(multipage, filename="all_Zstats.pdf", height = 9, width=9)
+        } else {
+            pl1 <- plots[1:4]
+            pl2 <- plots[5:8]
+            pl3 <- plots[9:12]
+            pl4 <- plots[13:16]
+            pl5 <- plots[17:20]
+            pl6 <- plots[21:22]
+            mpage1 <- ggpubr::ggarrange(plotlist = pl1)
+            mpage2 <- ggpubr::ggarrange(plotlist = pl2)
+            mpage3 <- ggpubr::ggarrange(plotlist = pl3)
+            mpage4 <- ggpubr::ggarrange(plotlist = pl4)
+            mpage5 <- ggpubr::ggarrange(plotlist = pl5)
+            mpage6 <- ggpubr::ggarrange(plotlist = pl6)
+            final_list <- list(mpage1, mpage2, mpage3, mpage4, mpage5, mpage6)
+
+            multipage <- ggpubr::ggarrange(plotlist = final_list, ncol=1, nrow=1)
+            ggpubr::ggexport(multipage, filename = "all_Zstats.pdf", height = 9, width=9)
         }
-        dev.off()
     }
     return(pres)
 }
