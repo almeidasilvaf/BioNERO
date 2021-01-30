50861223eaf7bc68f4f681ab6a455e46c4f14a75
almeidasilvaf <fabricio_almeidasilva@hotmail.com>
2021-01-30 11:44:30 -0300

    Included unit tests for grn_* functions.

--- R/grn_inference.R
+++ R/grn_inference.R
@@ -4,11 +4,16 @@
 #'
 #' @return A 2-column data frame containing node 1, node 2 and edge weight
 #' @export
+#' @rdname cormat_to_edgelist
+#' @export
 cormat_to_edgelist <- function(matrix) {
     edgelist <- matrix
     edgelist[lower.tri(edgelist, diag=TRUE)] <- NA
     edgelist <- na.omit(data.frame(as.table(edgelist), stringsAsFactors=FALSE))
     colnames(edgelist) <- c("Node1", "Node2", "Weight")
+    edgelist$Node1 <- as.character(edgelist$Node1)
+    edgelist$Node2 <- as.character(edgelist$Node2)
+    edgelist$Weight <- as.numeric(edgelist$Weight)
     return(edgelist)
 }
 
@@ -16,12 +21,14 @@ cormat_to_edgelist <- function(matrix) {
 #' Infer gene regulatory network with the Context Likelihood of Relatedness (CLR) algorithm
 #'
 #' @param exp Expression matrix with gene IDs as row names and samples as column names.
+#' @param estimator Entropy estimator to be used. One of "mi.empirical", "mi.mm", "mi.shrink", "mi.sg", "pearson", "spearman", or "kendall". Default: "pearson".
 #'
 #' @return A gene regulatory network represented as an edge list.
 #' @export
+#' @rdname grn_clr
 #' @importFrom minet build.mim clr
-grn_clr <- function(exp) {
-    mi_mat <- minet::build.mim(t(exp), estimator = "pearson")
+grn_clr <- function(exp, estimator = "pearson") {
+    mi_mat <- minet::build.mim(t(exp), estimator = estimator)
     grn <- minet::clr(mi_mat)
     grn_edges <- cormat_to_edgelist(grn)
     grn_edges <- grn_edges[grn_edges$Weight != 0, ]
@@ -32,12 +39,14 @@ grn_clr <- function(exp) {
 #' Infer gene regulatory network with the ARACNE algorithm
 #'
 #' @param exp Expression matrix with gene IDs as row names and samples as column names.
+#' @param estimator Entropy estimator to be used. One of "mi.empirical", "mi.mm", "mi.shrink", "mi.sg", "pearson", "spearman", or "kendall". Default: "spearman".
 #'
 #' @return A gene regulatory network represented as an edge list.
 #' @export
+#' @rdname grn_aracne
 #' @importFrom minet aracne
-grn_aracne <- function(exp) {
-    mi_mat <- minet::build.mim(t(exp), estimator = "pearson")
+grn_aracne <- function(exp, estimator = "spearman") {
+    mi_mat <- minet::build.mim(t(exp), estimator = estimator)
     grn <- minet::aracne(mi_mat, eps = 0.1)
     grn_edges <- cormat_to_edgelist(grn)
     grn_edges <- grn_edges[grn_edges$Weight != 0, ]
@@ -53,11 +62,21 @@ grn_aracne <- function(exp) {
 #'
 #' @return A gene regulatory network represented as an edge list.
 #' @importFrom GENIE3 GENIE3 getLinkList
+#' @rdname grn_genie3
+#' @export
+#' @importFrom GENIE3 GENIE3 getLinkList
 grn_genie3 <- function(exp, regulators = NULL, ...) {
-    grn <- GENIE3::GENIE3(as.matrix(exp), regulators = regulators)
+    grn <- GENIE3::GENIE3(as.matrix(exp), regulators = regulators, ...)
     edges <- GENIE3::getLinkList(grn)
+    edges[, 1] <- as.character(edges[, 1])
+    edges[, 2] <- as.character(edges[, 2])
+    edges[, 3] <- as.numeric(edges[, 3])
     return(edges)
 }
+
+
+
+
 
 
 
---
+++ tests/testthat/test_grn.R
@@ -0,0 +1,59 @@
+context("GRN inference")
+library(BioNERO)
+
+#----Simulate expression matrix----
+exp <- matrix(rnorm(10000), nrow=2000, ncol=50)
+rownames(exp) <- paste0("Gene", 1:nrow(exp))
+colnames(exp) <- paste0("Sample", 1:ncol(exp))
+
+#----Start tests----
+test_that("cormat_to_edgelist() converts correlation matrix to edge list", {
+    cor_mat <- cor(t(exp))
+    edgelist <- cormat_to_edgelist(cor_mat)
+    expect_equal(ncol(edgelist), 3)
+    expect_equal(colnames(edgelist), c("Node1", "Node2", "Weight"))
+})
+
+
+test_that("grn_clr() produces an edge list", {
+    grn <- grn_clr(exp)
+    expect_equal(colnames(grn), c("Node1", "Node2", "Weight"))
+    expect_equal(is.character(grn[,1]), TRUE)
+    expect_equal(is.character(grn[,2]), TRUE)
+    expect_equal(is.numeric(grn[,3]), TRUE)
+})
+
+
+test_that("grn_aracne() produces an edge list", {
+    grn <- grn_aracne(exp)
+    expect_equal(ncol(grn), 3)
+    expect_equal(colnames(grn), c("Node1", "Node2", "Weight"))
+    expect_equal(is.character(grn[,1]), TRUE)
+    expect_equal(is.character(grn[,2]), TRUE)
+    expect_equal(is.numeric(grn[,3]), TRUE)
+})
+
+
+test_that("grn_genie3() produces an edge list", {
+    grn <- grn_genie3(exp=exp, nTrees=100)
+    expect_equal(ncol(grn), 3)
+    expect_equal(is.character(grn[,1]), TRUE)
+    expect_equal(is.character(grn[,2]), TRUE)
+    expect_equal(is.numeric(grn[,3]), TRUE)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
